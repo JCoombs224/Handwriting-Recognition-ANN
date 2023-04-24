1271861d57d5fa4b9f025c294377984aee44174b
Jamison Coombs <58711684+JCoombs224@users.noreply.github.com>
2023-04-23 18:07:44 -0700

    updated class structure

--- main.cpp
+++ main.cpp
@@ -36,77 +36,6 @@ private:
     vector<vector<Node *>> layers;
     vector<vector<double>> inputs;
 
-public:
-    NeuralNetwork(string inputFile, string structureFile, double lrate)
-    {
-        // Load structure from file into the structure vector
-        ifstream file(structureFile);
-        if (file.is_open())
-        {
-            char c;
-            string val;
-            while (file >> c)
-            {
-                if (c == ',')
-                {
-                    structure.push_back(stoi(val));
-                    val = "";
-                }
-                else
-                {
-                    val += c;
-                }
-            }
-            structure.push_back(stoi(val));
-        }
-        file.close();
-
-        // Create layers with connections to each node based on structure
-        for (int i = 0; i < structure.size(); i++)
-        {
-            vector<Node *> layer;
-            for (int j = 0; j < structure[i]; j++)
-            {
-                if (i == 0)
-                {
-                    layer.push_back(new Node());
-                }
-                else
-                {
-                    layer.push_back(new Node(layers[i - 1]));
-                }
-            }
-            layers.push_back(layer);
-        }
-
-        // Load inputs from csv file into the inputs vector
-        file.open(inputFile);
-        if (file.is_open())
-        {
-            string line;
-            while (getline(file, line))
-            {
-                vector<double> input;
-                string value = "";
-                for (int i = 0; i < line.length(); i++)
-                {
-                    if (line[i] == ',')
-                    {
-                        input.push_back(stod(value));
-                        value = "";
-                    }
-                    else
-                    {
-                        value += line[i];
-                    }
-                }
-                input.push_back(stod(value));
-                this->inputs.push_back(input);
-            }
-        }
-        file.close();
-    }
-
     double sigmoid(double x)
     {
         return 1.0 / (1.0 + exp(-x));
@@ -115,35 +44,6 @@ public:
     double sigmoid_derivative(double collector)
     {
         return collector * (1.0 - collector);
-    }
-
-    // train the neural network using the inputs and structure with 1 output and print each epoch until the error is less than 0.05
-    void train(int num_epochs, double target_error = 0.05, double l_rate = 0.1)
-    {
-        int num_inputs = layers[0].size();
-
-        for (int epoch = 0; epoch < num_epochs; epoch++)
-        {
-            double epoch_error = 0;
-            for (auto row : inputs)
-            {
-                vector<double> expected;
-                feed_forward(row);
-                for (int j = 0; j < structure[structure.size() - 1]; j++)
-                {
-                    expected.push_back(row[num_inputs + j]);
-                    epoch_error += pow(row[num_inputs + j] - layers[layers.size() - 1][j]->collector, 2);
-                }
-                back_propagate(expected);
-                update_weights(l_rate);
-            }
-            if (epoch_error <= target_error)
-            {
-                cout << "Target error reached error " << epoch_error << endl;
-                return;
-            }
-            cout << ">Epoch=" << epoch << " l_rate=" << l_rate << " error=" << epoch_error << endl;
-        }
     }
 
     // update the weights
@@ -226,6 +126,105 @@ public:
             errors.clear(); // Clear the vector for the next iteration
         }
     }
+public:
+    NeuralNetwork(string inputFile, string structureFile)
+    {
+        // Load structure from file into the structure vector
+        ifstream file(structureFile);
+        if (file.is_open())
+        {
+            char c;
+            string val;
+            while (file >> c)
+            {
+                if (c == ',')
+                {
+                    structure.push_back(stoi(val));
+                    val = "";
+                }
+                else
+                {
+                    val += c;
+                }
+            }
+            structure.push_back(stoi(val));
+        }
+        file.close();
+
+        // Create layers with connections to each node based on structure
+        for (int i = 0; i < structure.size(); i++)
+        {
+            vector<Node *> layer;
+            for (int j = 0; j < structure[i]; j++)
+            {
+                if (i == 0)
+                {
+                    layer.push_back(new Node());
+                }
+                else
+                {
+                    layer.push_back(new Node(layers[i - 1]));
+                }
+            }
+            layers.push_back(layer);
+        }
+
+        // Load inputs from csv file into the inputs vector
+        file.open(inputFile);
+        if (file.is_open())
+        {
+            string line;
+            while (getline(file, line))
+            {
+                vector<double> input;
+                string value = "";
+                for (int i = 0; i < line.length(); i++)
+                {
+                    if (line[i] == ',')
+                    {
+                        input.push_back(stod(value));
+                        value = "";
+                    }
+                    else
+                    {
+                        value += line[i];
+                    }
+                }
+                input.push_back(stod(value));
+                this->inputs.push_back(input);
+            }
+        }
+        file.close();
+    }
+
+    // train the neural network
+    void train(int num_epochs, double target_error = 0.05, double l_rate = 0.1)
+    {
+        int num_inputs = layers[0].size();
+
+        for (int epoch = 0; epoch < num_epochs; epoch++)
+        {
+            double epoch_error = 0;
+            for (auto row : inputs)
+            {
+                vector<double> expected;
+                feed_forward(row);
+                for (int j = 0; j < structure[structure.size() - 1]; j++)
+                {
+                    expected.push_back(row[num_inputs + j]);
+                    epoch_error += pow(row[num_inputs + j] - layers[layers.size() - 1][j]->collector, 2);
+                }
+                back_propagate(expected);
+                update_weights(l_rate);
+            }
+            if (epoch_error <= target_error)
+            {
+                cout << "Target error reached error " << epoch_error << endl;
+                return;
+            }
+            cout << ">Epoch=" << epoch << " l_rate=" << l_rate << " error=" << epoch_error << endl;
+        }
+    }
 
     // run with a vector of inputs and print output
     void run(vector<double> inputs)
@@ -249,6 +248,7 @@ public:
         return output;
     }
 
+    // save the weights to a file
     void save(string filename)
     {
         ofstream file;
@@ -278,14 +278,16 @@ public:
 int main()
 {
     srand(time(NULL));
-    NeuralNetwork nn("input.csv", "network.csv", 0.1);
-    // train the neural network with the given input until the error is less than .05
+    NeuralNetwork nn("input.csv", "network.csv");
+    // train the neural network with the given input until the error is less than .03
     nn.train(10000, 0.03, 0.5);
+
     // print the output of the neural network
     vector<double> output = nn.get_output();
     for (int i = 0; i < output.size(); i++)
     {
         cout << output[i] << endl;
     }
+
     return 0;
 }
